1afd9f5bfdd3190566a8831e715392048f95d124
jjw <sucre123456@naver.com>
2022-03-22 11:12:54 +0900

    jjw

---
+++ Recipe/src/main/java/co/micol/prj/web/FrontController.java
@@ -0,0 +1,62 @@
+package co.micol.prj.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.micol.prj.border.command.BorderList;
+import co.micol.prj.comm.Command;
+import co.micol.prj.main.command.HomeCommand;
+
+
+
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private HashMap<String, Command> map = new HashMap<String, Command>();
+
+	public FrontController() {
+		super();
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+		map.put("/home.do", new HomeCommand()); // 첫 페이지
+		map.put("/borderList.do", new BorderList()); //공지사항 목록
+		
+		
+		
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String page = uri.substring(contextPath.length());
+
+		Command comm = map.get(page);
+		String viewPage = comm.exec(request, response);
+
+		if (!viewPage.endsWith(".do")) {
+			if (viewPage.startsWith("ajax:")) {
+				// aJax 처리
+				response.setContentType("text/html; charset=UTF-8");
+				response.getWriter().append(viewPage.substring(5)); // ajax: 총 5글자
+				return;
+			} else {
+				// viewPage = "WEB-INF/views/" + viewPage + ".jsp";
+				viewPage = viewPage + ".tiles";
+			}
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+}

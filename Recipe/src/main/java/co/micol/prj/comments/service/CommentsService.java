0d312f56ffb2988a637f0a0b690470345477fddf
Hyeonjinkk <guswls0975@naver.com>
2022-03-28 17:52:32 +0900

    댓글 기능 구현 준비

---
+++ Recipe/src/main/java/co/micol/prj/comments/service/CommentsService.java
@@ -0,0 +1,12 @@
+package co.micol.prj.comments.service;
+
+import java.util.List;
+
+public interface CommentsService {
+	
+	List<CommentsVO> selectCommentsList();
+	CommentsVO selectComments(CommentsVO vo);
+	int insertComments(CommentsVO vo);
+	int updateComments(CommentsVO vo);
+	int deleteComments(CommentsVO vo);
+}
